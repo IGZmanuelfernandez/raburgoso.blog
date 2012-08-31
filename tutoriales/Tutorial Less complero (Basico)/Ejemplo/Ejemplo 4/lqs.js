b5068b0b6004af8e9f85570bdaa31fd577a7eff6
Ricardo Andres Burgos Ocampo <ricardosflibre@gmail.com>
2012-08-30 23:38:45 -0500

    Tutorial de Less micro entradas

---
+++ tutoriales/Tutorial Less complero (Basico)/Ejemplo/Ejemplo 4/lqs.js
@@ -0,0 +1,9 @@
+function suma()
+{
+	var num1, num2, suma = 0;
+	num1 = parseInt(calculadora.num1.value);
+	num2 = parseInt(calculadora.num2.value);
+	suma = num1+num2;
+	calculadora.resul.value = suma; 
+
+}
